6944e055f50368827bfd48bc09d74bde8fee98e3
Aaron Earl <com_tech12@yahoo.com>
2017-11-19 15:48:40 -0800

    added data access and preped controllers

--- Homework6/Homework6/Controllers/HomeController.cs
+++ Homework6/Homework6/Controllers/HomeController.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Homework6.Models;
+using System.Net;
 
 namespace Homework6.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Global variables
+        /// </summary>
+        private AdventureWorks2014Entities db = new AdventureWorks2014Entities();
+        private int PageSize = 4;
+
+        /// <summary>
+        /// Homepage
+        /// </summary>
+        /// <returns> Returns Index.cshtml view</returns>
         public ActionResult Index()
         {
             return View();
@@ -25,6 +37,40 @@ namespace Homework6.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        /// <summary>
+        /// Queries Categories and displays subcategories based on query
+        /// Defaulst to Bikes and Page 1
+        /// </summary>
+        /// <param name="category"></param>
+        /// <param name="page"></param>
+        /// <returns> View of subcategories </returns>
+        [HttpGet]
+        public ActionResult Categories(string category = "Bikes", int page = 1)
+        {
+            return View(db.ProductSubcategories
+                        .Where(v => v.ProductCategory.Name.Contains(category))
+                        .OrderBy(p => p.ProductCategoryID)
+                        .Skip((page - 1) * PageSize)
+                        .Take(PageSize));
+
+        }
+
+        /// <summary>
+        /// Dispalys products based on Subcategory Queries
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <param name="page"></param>
+        /// <returns> View of Products based on ProductSubcategory ID</returns>
+        [HttpGet]
+        public ActionResult Products(int ID = 1, int page = 1)
+        {
+            return View(db.Products
+                        .Where(catID => catID.ProductSubcategoryID == ID)
+                        .OrderBy(PlatformID => PlatformID.ProductID)
+                        .Skip((PageSize - 1) * PageSize)
+                        .Take(PageSize));
         }
     }
 }
---
+++ Homework6/Homework6/DAL/DBContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Homework6.Models;
+
+namespace Homework6.DAL
+{
+    /// <summary>
+    /// Context for ProductCategories
+    /// </summary>
+    public class DBContext : DbContext
+    {
+        /// <summary>
+        /// Constructor for Category Context
+        /// </summary>
+        public DBContext() : base("name=MyDBContext") { }
+
+        /// <summary>
+        /// DBSet for Categories
+        /// </summary>
+        public virtual DbSet<ProductCategory> Category { get; set; }
+        public virtual DbSet<ProductCostHistory> CostHistory { get; set; }
+        public virtual DbSet<ProductDescription> Description { get; set; }
+        public virtual DbSet<ProductInventory> Inventory { get; set; }
+        public virtual DbSet<ProductListPriceHistory> PriceListHistory { get; set; }
+        public virtual DbSet<ProductModel> Model { get; set; }
+        public virtual DbSet<ProductModelIllustration> Illustration { get; set;}
+        public virtual DbSet<ProductModelProductDescriptionCulture> ProductDescriptionCulture { get; set; }
+        public virtual DbSet<ProductPhoto> Photo { get; set; }
+        public virtual DbSet<ProductProductPhoto> ProductProductPhoto { get; set; }
+        public virtual DbSet<ProductReview> Review { get; set; }
+        public virtual DbSet<ProductSubcategory> Subcategory { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
+        public virtual DbSet<ScrapReason> ScrapReason { get; set; }
+        public virtual DbSet<UnitMeasure> UnitMeasuer { get; set; }
+        public virtual DbSet<TransactionHistory> TransactionHistory { get; set; }
+        public virtual DbSet<TransactionHistoryArchive> TransactionHistoryArchive { get; set; }
+        public virtual DbSet<WorkOrder> WorkOrder { get; set; }
+        public virtual DbSet<WorkOrderRouting> WorkOrderRouting { get; set; }
+        public virtual DbSet<BillOfMaterial> BillOfMaterials { get; set; }
+        public virtual DbSet<Culture> Culture { get; set; }
+        public virtual DbSet<ProductDocument> ProductDocument { get; set; }
+    }
+}
